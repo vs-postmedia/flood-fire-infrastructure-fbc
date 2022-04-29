1fdb277338d0dd90421066761287a7586d0c73b5
Nathan Grifiths <njgriffiths@gmail.com>
2022-04-29 13:07:30 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import colours from './css/colors.css';
 import fonts from './css/fonts.css';
 import css from './css/main.css';
 
+// IMAGES
+import richmond from './images/richmond-legend.png';
+import chilliwack from './images/chilliwack.png';
 
 // JS
-// const init = async () => {
+const init = async () => {
+	const img_rich = document.getElementById('richmond');
+	img_rich.src = richmond;
 
-// };
+	const img_chill = document.getElementById('chilliwack');
+	img_chill.src = chilliwack;
+};
 
-// init();
+init();
